7ffde75bf180339878ca46cffcf526a5a1a974bb
Cristianps1988 <cristianps_1@hotmail.com>
2023-02-09 07:08:54 -0500

    eventlistener added

--- js/app.js
+++ js/app.js
@@ -27,3 +27,25 @@ const ui = new UI();
 document.addEventListener('DOMContentLoaded', () =>{
     ui.llenarOpciones();
 });
+
+eventListeners();
+
+function eventListeners(){
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+
+}
+
+function cotizarSeguro(e){
+    e.preventDefault();
+
+    const marca = document.querySelector('#marca').value;
+    const year = document.querySelector('#year').value;
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+    if(marca === '' || year === '' || tipo === ''){
+        console.log('Falta algo');
+    } else{
+        console.log('todo en orden');
+    }
+}
