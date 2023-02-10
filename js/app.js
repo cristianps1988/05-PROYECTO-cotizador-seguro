00d59ee93bcf95e2ee81d13eb34c431c4a3b10e5
Cristianps1988 <cristianps_1@hotmail.com>
2023-02-10 06:31:11 -0500

    funcion de calcular precio del seguro

--- js/app.js
+++ js/app.js
@@ -6,6 +6,44 @@ function Seguro(marca, year, tipo){
     this.tipo = tipo
 }
 
+Seguro.prototype.cotizarSeguro = function(){
+    // 1 = 1.15
+    // 2 = 1.05
+    // 3 = 1.35
+    let resultado;
+    const base = 2000;
+    switch (this.marca) {
+        case '1':
+            resultado = base * 1.15;
+            break;
+        case '2':
+            resultado = base * 1.05;
+            break;
+        case '3':
+            resultado = base * 1.35;
+            break;
+        default:
+            break;
+    }
+
+    // leer el year
+    const diferencia = new Date().getFullYear() - this.year;
+
+    // por cada year de antiguedad, el costo reduce 3%
+    resultado -= ((diferencia * 3) * resultado) / 100;
+
+    // si el seguro es basico se multiplica por 30%
+    // si es completo se multiplica por 505
+
+    if(this.tipo === 'basico'){
+        resultado *= 1.3;
+    } else{
+        resultado *= 1.5;
+    }
+
+    return resultado
+}
+
 function UI(){};
 
 // llenar las opciones de los years
@@ -22,10 +60,33 @@ UI.prototype.llenarOpciones = () => {
     }
 }
 
+// muestra alertas en pantalla
+UI.prototype.mostrarMensaje = (mensaje, tipo) => {
+    const div = document.createElement('div');
+    
+    if(tipo === 'error'){
+        div.classList.add('error')
+    } else{
+        div.classList.add('correcto')
+    }
+    div.classList.add('mensaje', 'mt-10');
+    div.textContent = mensaje;
+
+    // insertar en el HTML
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+
+    setTimeout(() => {
+        div.remove();
+    }, 3000);
+}
+
+
+// instanciar ui
 const ui = new UI();
 
 document.addEventListener('DOMContentLoaded', () =>{
-    ui.llenarOpciones();
+    ui.llenarOpciones(); // llena el select con los years
 });
 
 eventListeners();
@@ -44,8 +105,15 @@ function cotizarSeguro(e){
     const tipo = document.querySelector('input[name="tipo"]:checked').value;
 
     if(marca === '' || year === '' || tipo === ''){
-        console.log('Falta algo');
-    } else{
-        console.log('todo en orden');
+        ui.mostrarMensaje('Todos los campos son obligatorios', 'error');
+        return;
     }
+    ui.mostrarMensaje('Cotizando', 'exito');
+
+    // instanciar seguro
+    const seguro = new Seguro(marca, year, tipo);
+    seguro.cotizarSeguro();
+
+    // utilizar el proto que va a cotizar
+
 }
